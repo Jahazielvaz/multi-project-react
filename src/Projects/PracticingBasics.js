19d3755756dc741722a1c8893163eed9b3512d2d
Jahaziel <jahaziel.vaz@gmail.com>
2019-01-29 22:00:36 -0700

    First commit

---
+++ src/Projects/PracticingBasics.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './Projects.css';
+
+const PracticingBasics = () => {
+  return(
+    <div>
+      <h1>WELCOME TO OUR BASICS PROJECT</h1>
+      <h2>Please follow our task list and execute each flawlessly</h2>
+
+      <section>
+        <ul>
+          <li>Yo</li>
+        </ul>
+      </section>
+    </div>
+  )
+}
+
+export default PracticingBasics;
